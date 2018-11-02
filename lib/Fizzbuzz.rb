86a3fa4733c3c8052ad435defd9f3f80509b78a6
Merce Bauza <merce.bauza@gmail.com>
2018-11-02 10:19:56 +0000

    refractor and restructure the project

--- lib/Fizzbuzz.rb
+++ lib/Fizzbuzz.rb
@@ -1,37 +1,30 @@
-require "Fizzbuzz/version"
+class Fizzbuzz
+  def print(number)
+    puts play(number)
+  end
 
-module Fizzbuzz
-	
-	def self.printFizzBuzz(number)
-	 puts playFizzBuzz(number)
-	end
+  def play(number)
+    word = ["Fizz", "Buzz"]
+    return word[0]+word[1] if divisible_by_3_and_5?(number)
+    return word[1] if divisible_by_5?(number)
+    return word[0] if divisible_by_3?(number)
+    number
+  end
 
-	def self.playFizzBuzz(number)
-	word = ["Fizz", "Buzz"]
-	return word[0]+word[1] if divisible_by_3_and_5(number)
-	return word[1] if divisible_by_5(number)
-	return word[0] if divisible_by_3(number)
-	return number
-	end	
+  def divisible_by_nums?(number, nums)
+    nums.all? {|i| number % i == 0}
+  end
 
-	def self.divisible_by_nums(number, nums)
-	   nums.each do |i|
-		if (number % i != 0) 
-		return false 
-		end
-	   end
-	return true 
-	end
+  def divisible_by_3?(number)
+    divisible_by_nums?(number, [3])
+  end
 
-	def self.divisible_by_3(number)
-	divisible_by_nums(number, [3])
-	end  
+  def divisible_by_5?(number)
+    divisible_by_nums?(number, [5])
+  end
 
-	def self.divisible_by_5(number)
-	divisible_by_nums(number, [5])
-	end  
-
-	def self.divisible_by_3_and_5(number)
-        divisible_by_nums(number, [3,5])
-        end
+  def divisible_by_3_and_5?(number)
+    divisible_by_nums?(number, [3,5])
+  end
 end
+
--- spec/Fizzbuzz_spec.rb
+++ spec/Fizzbuzz_spec.rb
@@ -1,46 +1,48 @@
+require "rspec"
+require "Fizzbuzz"
+
 RSpec.describe Fizzbuzz do
-  it "has a version number" do
-    expect(Fizzbuzz::VERSION).not_to be nil
-  end
+
+  let (:fizz) { Fizzbuzz.new }
 
   it "3 is divisible by 3" do
-    expect(Fizzbuzz.divisible_by_3(3)).to eq(true)
+    expect(fizz.divisible_by_3?(3)).to eq(true)
   end
 
   it "4 is not divisible by 3" do
-    expect(Fizzbuzz.divisible_by_3(4)).to eq(false)
+    expect(fizz.divisible_by_3?(4)).to eq(false)
   end
 
   it "5 is divisible by 5" do
-    expect(Fizzbuzz.divisible_by_5(5)).to eq(true)
+    expect(fizz.divisible_by_5?(5)).to eq(true)
   end
 
   it "4 is not divisible by 5" do
-    expect(Fizzbuzz.divisible_by_5(4)).to eq(false)
+    expect(fizz.divisible_by_5?(4)).to eq(false)
   end
 
    it "15 is divisible by 3 and 5" do
-    expect(Fizzbuzz.divisible_by_3_and_5(15)).to eq(true)
+    expect(fizz.divisible_by_3_and_5?(15)).to eq(true)
   end
 
-  it "16 is not divisible by 3 and 5" do
-    expect(Fizzbuzz.divisible_by_3_and_5(16)).to eq(false)
+  it "6 is not divisible by 3 and 5" do
+    expect(fizz.divisible_by_3_and_5?(6)).to eq(false)
   end
 
   it "print Fizz when divisible by 3" do
-    expect(Fizzbuzz.playFizzBuzz(6)).to eq("Fizz")
+    expect(fizz.play(6)).to eq("Fizz")
   end
 
   it "print Buzz when divisible by 5" do
-    expect(Fizzbuzz.playFizzBuzz(10)).to eq("Buzz")
+    expect(fizz.play(25)).to eq("Buzz")
   end
 
   it "print FizzBuzz when divisible by 15" do
-    expect(Fizzbuzz.playFizzBuzz(30)).to eq("FizzBuzz")
+    expect(fizz.play(30)).to eq("FizzBuzz")
   end
 
   it "print number when not divisible by 3, 5, 15" do
-    expect(Fizzbuzz.playFizzBuzz(7)).to eq(7)
+    expect(fizz.play(7)).to eq(7)
   end
 
 end
